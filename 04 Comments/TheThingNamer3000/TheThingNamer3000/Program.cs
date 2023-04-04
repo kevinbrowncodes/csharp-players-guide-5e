21e48533224a1f5e0a224cb1f16d109d5cb298cc
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-04-04 10:36:50 -0400

    0401 Challenge The Thing Namer 3000

---
+++ 04 Comments/TheThingNamer3000/TheThingNamer3000/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+
+// Thing type
+string a = Console.ReadLine();
+
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+
+// Thing description
+string b = Console.ReadLine();
+
+// text "Doom"
+string c = "Doom"; /* Modified to fix the "of of" bug */
+
+// text "3000"
+string d = "3000";
+
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
