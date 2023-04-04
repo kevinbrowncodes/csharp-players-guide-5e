b2521d2642a0fe10b89f07c13975752b15ef746f
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-04-04 07:59:20 -0400

    0305 Challenge The Makings of a Programmer

---
+++ 03 Hello World/TheMakingsOfAProgrammer/TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("We've all got both light");
+Console.WriteLine("and dark inside of us.");
+Console.WriteLine("What matters is the part");
+Console.WriteLine("we choose to act on.");
+Console.WriteLine("That's who we really are.");
+
