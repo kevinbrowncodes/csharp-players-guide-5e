4a6f4ad5e90c3e5ac28bfeebb539291fcec9f39e
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-04-05 10:11:03 -0400

    0703 The Triangle Farmer

---
+++ 07 Math/TheTriangleFarmer/TheTriangleFarmer/Program.cs
@@ -0,0 +1,13 @@
+﻿double areaTriangle;
+int baseTriangle;
+int heightTriangle;
+
+Console.WriteLine("Enter triangle base: ");
+baseTriangle = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter triangle height: ");
+heightTriangle = Convert.ToInt32(Console.ReadLine());
+
+areaTriangle = (baseTriangle * heightTriangle) / 2;
+
+Console.WriteLine("The area of the triangle is " + areaTriangle);
