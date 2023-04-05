e3db4dbad8c62e91f48dc4a9598fa986d3cf338c
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-04-05 10:25:29 -0400

    0707 Challenge The Dominion of Kings

---
+++ 07 Math/TheDominionOfKings/TheDominionOfKings/Program.cs
@@ -0,0 +1,21 @@
+﻿int estatePoint = 1;
+int duchyPoint = 3;
+int provincePoint = 6;
+int estateNum;
+int duchyNum;
+int provinceNum;
+int total;
+
+Console.WriteLine("Number of estates: ");
+estateNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Number of duchies: ");
+duchyNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Number of provinces: ");
+provinceNum = Convert.ToInt32(Console.ReadLine());
+
+total = (estatePoint * estateNum) + (duchyPoint * duchyNum) + (provincePoint * provinceNum);
+
+Console.WriteLine("Total: " + total);
+
