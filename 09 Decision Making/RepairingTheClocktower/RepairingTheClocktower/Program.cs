99d6f48e28eb27b678d3c165d0d0fcbfde2bf649
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-04-05 13:13:34 -0400

    0905 Challenge Repairing the Clocktower

---
+++ 09 Decision Making/RepairingTheClocktower/RepairingTheClocktower/Program.cs
@@ -0,0 +1,27 @@
+﻿int num;
+string word;
+
+/* Take a number as input from the console */
+#region
+Console.WriteLine("Input number: ");
+num = Convert.ToInt32(Console.ReadLine());
+#endregion
+
+/* Display the word "Tick" if the number is even. 
+ * Display the word "Tock" if the number is odd. */
+#region
+if (num % 2 == 0)
+{
+    word = "Tick";
+}
+else if (num % 2 > 0)
+{
+    word = "Tock";
+}
+else
+{
+    word = "Error!";
+}
+
+Console.WriteLine(word);
+#endregion
