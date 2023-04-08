a4ab8ed1fe92136ff3eae1973b6ec4b7ad6b6048
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-04-08 13:15:52 -0400

    1308 Challenge Countdown

---
+++ 13 Methods/Countdown/Countdown/Program.cs
@@ -0,0 +1,23 @@
+﻿int numInput = 10;
+
+/* A countdown loop */
+for(int x = numInput; x > 0; x--)
+{
+    Console.WriteLine(x);
+}
+
+/* A countdown loop with no loops */
+Countdown(numInput);
+
+void Countdown(int numInput)
+{
+    // base case
+    if(numInput == 0)
+    {
+        return;
+    }
+
+    Console.WriteLine(numInput);
+    numInput--;
+    Countdown(numInput);
+}
