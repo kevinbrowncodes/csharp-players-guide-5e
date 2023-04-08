f9783e8aabc99100600b72216257f4721eb53105
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-04-08 13:02:50 -0400

    1307 Challenge Taking a Number

---
+++ 13 Methods/TakingANumber/TakingANumber/Program.cs
@@ -0,0 +1,38 @@
+﻿/* method with signature int AskForNumber(string text) */
+#region
+int resultNum = AskForNumber("What is the airspeed velocity of an unladen swallow?");
+Console.WriteLine("The airspeed velocity of an unladen swallow is " + resultNum + "mph." + "\n");
+
+int AskForNumber(string ask) {
+    Console.WriteLine(ask);
+    int response = Convert.ToInt32(Console.ReadLine());
+
+    return response;
+}
+#endregion
+
+/* method with signature int AskForNumberInRange(string text, int min, int max) */
+#region
+int resultNumInRange = AskForNumberInRange("What is number in range:", 50, 100);
+Console.WriteLine(resultNumInRange + " is within range.");
+
+int AskForNumberInRange(string text, int min, int max){
+    int numRangeInput;
+
+    Console.WriteLine($"{text} ({min},{max})");
+    numRangeInput = Convert.ToInt32(Console.ReadLine());
+
+    while(!(numRangeInput >= min && numRangeInput <= 100))
+    {
+        Console.WriteLine($"{text} ({min},{max})");
+        numRangeInput = Convert.ToInt32(Console.ReadLine());
+
+        if((numRangeInput >= min && numRangeInput <= 100))
+        {
+            break;
+        }
+    }
+
+    return numRangeInput;
+}
+#endregion
