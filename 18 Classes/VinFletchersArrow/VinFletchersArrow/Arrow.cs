007677f00c321da2b7cf7b2673343b5024e84cd3
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-04-15 09:58:00 -0400

    1804 Challenge Vin Fletcher's Arrows

---
+++ 18 Classes/VinFletchersArrow/VinFletchersArrow/Arrow.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace VinFletchersArrow
+{
+	/* Define a new Arrow class with fields for arrowhead type, fletching type,
+	* and length. */  
+	public class Arrow
+	{
+        public enum Arrowhead
+        {
+            Steel,
+            Wood,
+            Obsidian
+        }
+
+        public enum Fletching
+        {
+            Plastic,
+            Turkey,
+            Goose
+        }
+
+        Arrowhead arrowhead;
+        Fletching fletching;
+		int length;
+
+		public Arrow(Arrowhead arrowhead, int length, Fletching fletching)
+		{
+			this.arrowhead = arrowhead;
+			this.length = length;
+            this.fletching = fletching;
+        }
+
+        public float GetCost(Arrow arrow)
+        {
+            float costArrowhead;
+            float costLength;
+            float costFletching;
+            float costTotal;
+
+            // arrowhead money
+            if(Arrowhead.Wood == arrow.arrowhead)
+            {
+                costArrowhead = 3;
+            }
+            else if(Arrowhead.Obsidian == arrow.arrowhead)
+            {
+                costArrowhead = 5;
+            }
+            else if (Arrowhead.Steel == arrow.arrowhead)
+            {
+                costArrowhead = 10;
+            }
+            else
+            {
+                costArrowhead = 0;
+            }
+
+            // shaft money
+            costLength = length * 0.05F;
+
+            // fletching money
+            if (Fletching.Goose == arrow.fletching)
+            {
+                costFletching = 3;
+            }
+            else if (Fletching.Turkey == arrow.fletching)
+            {
+                costFletching = 5;
+            }
+            else if (Fletching.Plastic == arrow.fletching)
+            {
+                costFletching = 10;
+            }
+            else
+            {
+                costFletching = 0;
+            }
+
+            costTotal = (costArrowhead + costLength + costFletching);
+
+            return costTotal;
+        }
+
+
+	}
+}
+
---
+++ 18 Classes/VinFletchersArrow/VinFletchersArrow/Program.cs
@@ -0,0 +1,38 @@
+﻿using VinFletchersArrow;
+using static VinFletchersArrow.Arrow;
+
+Arrowhead arrowhead;
+int shaft = 0;
+Fletching fletching;
+float costArrow;
+
+/* Allow a user to pick the arrowhead, fletching, and length 
+ * and then create a new Arrow instance */
+// pick a arrowhead
+Console.WriteLine($"Pick an arrowhead: " +
+                    $"{Arrowhead.Steel}, " +
+                    $"{Arrowhead.Wood}, " +
+                    $"{Arrowhead.Obsidian}");
+string strArrowhead = Console.ReadLine();
+arrowhead = (Arrowhead)Enum.Parse(typeof(Arrowhead), strArrowhead);
+
+// pick a shaft
+while(shaft < 60 || shaft > 100)
+{
+    Console.WriteLine($"Pick a shaft between 60cm and 100cm: ");
+    shaft = Convert.ToInt32(Console.ReadLine());
+}
+
+// pick a fletching
+Console.WriteLine($"Pick a fletching: " +
+                    $"{Fletching.Plastic}, " +
+                    $"{Fletching.Turkey}, " +
+                    $"{Fletching.Goose}");
+string strFletching = Console.ReadLine();
+fletching = (Fletching)Enum.Parse(typeof(Fletching), strFletching);
+
+Arrow arrow = new Arrow(arrowhead, shaft, fletching);
+
+costArrow = arrow.GetCost(arrow);
+
+Console.WriteLine($"\nThe cost of the arrow is ${costArrow} gold.");
