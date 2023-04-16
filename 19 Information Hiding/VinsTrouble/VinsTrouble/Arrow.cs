de8d6beff9a1e6ac56a1c3bed31d1b29bd491bfe
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-04-16 10:04:55 -0400

    1903 Challenge Vin's Trouble

---
+++ 19 Information Hiding/VinsTrouble/VinsTrouble/Arrow.cs
@@ -0,0 +1,95 @@
+﻿using System;
+namespace VinFletchersArrow
+{
+    /* Modify your Arrow class to have pricate instead of public fields */
+    public class Arrow
+    {
+        public enum Arrowhead
+        {
+            Steel,
+            Wood,
+            Obsidian
+        }
+
+        public enum Fletching
+        {
+            Plastic,
+            Turkey,
+            Goose
+        }
+
+        private Arrowhead arrowhead;
+        private Fletching fletching;
+        private int length;
+        private float cost;
+
+        public Arrow(Arrowhead arrowhead, int length, Fletching fletching)
+        {
+            this.arrowhead = arrowhead;
+            this.length = length;
+            this.fletching = fletching;
+
+            cost = GetCost(this.arrowhead, this.length, this.fletching);
+        }
+
+        /* Add in getter methods for each of the fields that you have */
+        public Arrowhead GetArrowHead() => arrowhead;
+        public Fletching GetFletching() => fletching;
+        public int GetLength() => length;
+        public float GetCost() => cost;
+
+        private float GetCost(Arrowhead arrowhead, int length, Fletching fletching)
+        {
+            float costArrowhead;
+            float costLength;
+            float costFletching;
+            float costTotal;
+
+            // arrowhead money
+            if (Arrowhead.Wood == arrowhead)
+            {
+                costArrowhead = 3;
+            }
+            else if (Arrowhead.Obsidian == arrowhead)
+            {
+                costArrowhead = 5;
+            }
+            else if (Arrowhead.Steel == arrowhead)
+            {
+                costArrowhead = 10;
+            }
+            else
+            {
+                costArrowhead = 0;
+            }
+
+            // shaft money
+            costLength = length * 0.05F;
+
+            // fletching money
+            if (Fletching.Goose == fletching)
+            {
+                costFletching = 3;
+            }
+            else if (Fletching.Turkey == fletching)
+            {
+                costFletching = 5;
+            }
+            else if (Fletching.Plastic == fletching)
+            {
+                costFletching = 10;
+            }
+            else
+            {
+                costFletching = 0;
+            }
+
+            costTotal = (costArrowhead + costLength + costFletching);
+
+            return costTotal;
+        }
+
+
+    }
+}
+
---
+++ 19 Information Hiding/VinsTrouble/VinsTrouble/Program.cs
@@ -0,0 +1,47 @@
+﻿using VinFletchersArrow;
+using static VinFletchersArrow.Arrow;
+
+Arrowhead arrowhead;
+int shaft = 0;
+Fletching fletching;
+float costArrow;
+
+/* Allow a user to pick the arrowhead, fletching, and length 
+ * and then create a new Arrow instance */
+// pick a arrowhead
+Console.WriteLine($"Pick an arrowhead: " +
+                    $"{Arrowhead.Steel}, " +
+                    $"{Arrowhead.Wood}, " +
+                    $"{Arrowhead.Obsidian}");
+string strArrowhead = Console.ReadLine();
+arrowhead = (Arrowhead)Enum.Parse(typeof(Arrowhead), strArrowhead);
+
+// pick a shaft
+while (shaft < 60 || shaft > 100)
+{
+    Console.WriteLine($"Pick a shaft between 60cm and 100cm: ");
+    shaft = Convert.ToInt32(Console.ReadLine());
+}
+
+// pick a fletching
+Console.WriteLine($"Pick a fletching: " +
+                    $"{Fletching.Plastic}, " +
+                    $"{Fletching.Turkey}, " +
+                    $"{Fletching.Goose}");
+string strFletching = Console.ReadLine();
+fletching = (Fletching)Enum.Parse(typeof(Fletching), strFletching);
+
+Arrow arrow = new Arrow(arrowhead, shaft, fletching);
+
+// making the arrow field private prevents updating arrow's length
+// arrow.length = 60;
+
+// costArrow = arrow.GetCost(arrow);
+
+Console.WriteLine($"\nThe cost of the arrow is ${arrow.GetCost()} gold.");
+
+/* Validation */
+// Arrowhead = Wood = 3
+// Length = 60 => (60 * .05) => 3
+// Fletching = Goose = 3
+// Cost = 9
