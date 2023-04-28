2ea799ee225c8f24b42a32452562bdc5005e3ede
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-04-28 10:09:22 -0400

    2401 Boss Bottle The Point

---
+++ 24 Catacombs of the Class/ThePoint/ThePoint/Point.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace ThePoint
+{
+	/* Define a new Point class with properties for X and Y */
+	public class Point
+	{
+		private int _coordX;
+		private int _coordY;
+
+
+		/* Add a parameterless constructor to create a point at origin (0,0); */
+		public Point()
+		{
+			_coordX = 0;
+            _coordY = 0;
+        }
+
+
+		/* Add a constructor to create a point from a specific x- and y- coordinate */
+        public Point(int coordX, int coordY)
+		{
+			_coordX = coordX;
+			_coordY = coordY;
+		}
+
+        // properties
+        public int CoordX
+		{
+			get
+			{
+				return _coordX;
+			}
+			set
+			{
+				_coordX = value;
+
+            }
+		}
+
+		public int CoordY
+		{
+			get
+			{
+				return _coordY;
+			}
+			set
+			{
+				_coordY = value;
+            }
+		}
+    }
+}
+
---
+++ 24 Catacombs of the Class/ThePoint/ThePoint/Program.cs
@@ -0,0 +1,23 @@
+﻿using ThePoint;
+
+int coordXInput;
+int coordYInput;
+Point point;
+
+coordXInput = 2;
+coordYInput = 3;
+
+point = new Point(coordXInput, coordYInput);
+
+Console.WriteLine($"({point.CoordX},{point.CoordY})");
+
+coordXInput = -4;
+coordYInput = 0;
+
+point = new Point(coordXInput, coordYInput);
+
+Console.WriteLine($"({point.CoordX},{point.CoordY})");
+
+/* Questions */
+// Are your X and Y properties immutable? Why did you choose what you did?
+/// Yes, my X and Y properties are immutable since they cannot be changed.
