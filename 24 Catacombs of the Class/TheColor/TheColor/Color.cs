4e5f8b6e0b8a6cae564d164f5b9542163a712795
kevinbrowncodes <81748953+kevinbrowncodes@users.noreply.github.com>
2023-04-29 09:56:48 -0400

    2401 Boss Battle The Color

---
+++ 24 Catacombs of the Class/TheColor/TheColor/Color.cs
@@ -0,0 +1,69 @@
+﻿namespace TheColor
+{
+	/* Define a new Color class with properties for its red, green, and blue channels */
+	public class Color
+	{
+		// fields
+		private int _r;
+		private int _g;
+		private int _b;
+
+		// constructors
+		public Color()
+		{
+			_r = 0;
+			_g = 0;
+			_b = 0;
+		}
+
+		public Color(int red, int green, int blue)
+		{
+			_r = red;
+			_g = green;
+			_b = blue;
+		}
+
+		// properties
+		public int R
+		{
+			get
+			{
+				return _r;
+			}
+		}
+
+		public int G
+		{
+			get
+			{
+				return _g;
+			}
+		}
+
+		public int B
+		{
+			get
+			{
+				return _b;
+			}
+		}
+		/* create static properties to define eight colors */
+		// White
+		public static Color White { get; } = new Color(255, 255, 255);
+		// Black
+		public static Color Black { get; } = new Color(0, 0, 0);
+		// Red
+		public static Color Red { get; } = new Color(255, 0, 0);
+		// Orange
+		public static Color Orange { get; } = new Color(255, 165, 0);
+		// Yellow
+		public static Color Yellow {get;} = new Color(255, 255, 0);
+		// Green
+		public static Color Green { get; } = new Color(0, 128, 0);
+		// Blue
+		public static Color Blue { get; } = new Color(0, 0, 255);
+		// Purple
+		public static Color Purple { get; } = new Color(128, 0, 128);
+    }
+}
+
---
+++ 24 Catacombs of the Class/TheColor/TheColor/Program.cs
@@ -0,0 +1,17 @@
+﻿using TheColor;
+
+/* make two Color-types variables */
+Color colorRandom;
+Color colorFixed;
+
+/* use a constructor to create a color instance 
+ * and use a static property for the other */
+colorRandom = new Color(1, 1, 1);
+colorFixed = Color.Yellow;
+
+/* display each of their red, green, and blue
+ * channels */
+Console.WriteLine("Color 1");
+Console.WriteLine($"R:{colorRandom.R} G:{colorRandom.G} B:{colorRandom.B}");
+Console.WriteLine("Color 2");
+Console.WriteLine($"R:{colorFixed.R} G:{colorFixed.G} B:{colorFixed.B}");
